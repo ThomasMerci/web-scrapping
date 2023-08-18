93cf3ed2c813f6c8c276afb92f213c2fb192a134
patatefroide <mercierthomas2022@gmail.com>
2023-08-18 11:12:21 +0200

    docker fonctionnel

--- Crawler.py
+++ Crawler.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 from urllib.request import Request,urlopen
+from sqlalchemy import create_engine
 url = "https://www.sfds.asso.fr/fr/n/506-consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
 page = urlopen(url)
-html = bs(page, 'html')
-
+html = bs(page, "html.parser")
 #print(html)
-titre = html.findAll('td', {'class': 'jobLabel'})
-for element in titre:
- print(element.get_text())
 
-
+#titre = html.findAll('td', {'class': 'jobLabel'})
+#for element in titre:
+# print(element.get_text())
 allJobs = html.findAll('div', class_='job')
 titre_offre = []
 date_offre = []
@@ -29,19 +29,20 @@ for job in allJobs:
  
 #Print all Titles Jobs
 print(titre_offre)
-
 #Print all Date Pub
-print(date_offre)
-
+#print(date_offre)
 #Print all Resume
-print(resume_offre)
-
-
-import pandas as pd
+#print(resume_offre)
 dfJobs = pd.DataFrame({
  'TITRE_OFFRE': titre_offre,
  'DATE_PUB_OFFRE': date_offre,
  'RESUME_OFFRE': resume_offre
 })
-dfJobs.to_csv('jobs.csv')
-print(dfJobs.head())
+# create sqlalchemy engine
+user = 'root'
+passw = 'supersecret'
+host = 'my-mysql'
+port = 3306  
+database = 'jobs_scraping'
+engine = create_engine('mysql+pymysql://' + user + ':' + passw + '@' + host + ':' + str(port) + '/' + database , echo=False)
+dfJobs.to_sql('jobs', con = engine, if_exists = 'append',index= False, chunksize= 1000)
